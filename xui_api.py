30deb5e11e0fb11d6331b11c8a2b61d17c9bc680
mhmdbytsyd86-collab <mhmdbytsyd86@gmail.com>
2026-07-21 16:14:37 +0330

    Update xui_api.py

--- xui_api.py
+++ xui_api.py
@@ -1,10 +1,33 @@
 import os
 import requests
 
+
 PANEL_URL = os.getenv("PANEL_URL")
 PANEL_USERNAME = os.getenv("PANEL_USERNAME")
 PANEL_PASSWORD = os.getenv("PANEL_PASSWORD")
 
 
-def test_panel():
-    print("Panel:", PANEL_URL)
+session = requests.Session()
+
+
+def login_panel():
+    url = f"{PANEL_URL}/login"
+
+    data = {
+        "username": PANEL_USERNAME,
+        "password": PANEL_PASSWORD
+    }
+
+    response = session.post(url, data=data)
+
+    if response.status_code == 200:
+        return True
+
+    return False
+
+
+def get_panel_status():
+    if login_panel():
+        return "✅ اتصال به پنل موفق بود"
+    else:
+        return "❌ اتصال به پنل ناموفق بود"
