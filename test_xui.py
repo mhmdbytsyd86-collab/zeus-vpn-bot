6564869f6a8016e3a9ac899621650f03593935ac
mhmdbytsyd86-collab <mhmdbytsyd86@gmail.com>
2026-07-21 19:18:23 +0330

    Update test_xui.py

--- test_xui.py
+++ test_xui.py
@@ -4,18 +4,12 @@ from xui_api import XUI
 xui = XUI(
     "https://3x-ui-production-1903.up.railway.app",
     "mobin",
-    "Mm009900**"
+    "Mm00990099"
 )
 
+
+print("login start")
 
 xui.login()
 
-
-user = xui.create_client(
-    email="telegram_12345",
-    volume_gb=100,
-    days=30
-)
-
-
-print(user)
+print("login ok")
